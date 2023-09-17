32b304129cd21662a0e640c22612e7b52fe86aa8
Cold Dev <96265196+icemedia001@users.noreply.github.com>
2023-09-17 01:20:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,106 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Middleware for parsing JSON and URL-encoded data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// MongoDB connection setup
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('MongoDB connected successfully.');
+});
 
-mongoose.set("strictQuery", true);
-mongoose.connect(process.env.MONGODB_URI);
-
-const userSchema = new mongoose.Schema({
+// Define a Mongoose model
+const User = mongoose.model('User', {
   fullName: String,
   section: String,
   agreeToTerms: Boolean,
 });
 
-const User = mongoose.model("User", userSchema);
+// Middleware
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("home");
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the CRUD application!');
 });
 
-// Corrected route definition for form submission
-app.post("/submit-form", async (req, res) => {
+// Create a new user
+app.post('/users', async (req, res) => {
   try {
     const { fullName, section, agreeToTerms } = req.body;
-    
-    if (!agreeToTerms) {
-      return res.status(400).json({
-        error: "You must agree to terms and conditions."
-      });
-    }
-
-    // Create a new User document and save it to the database
-    const user = new User({
-      fullName,
-      section,
-      agreeToTerms,
-    });
-
-    await user.save();
-
-    res.status(200).json({
-      message: "Form submitted successfully"
-    });
+    const user = new User({ fullName, section, agreeToTerms });
+    const newUser = await user.save();
+    res.status(201).json(newUser);
   } catch (error) {
-    console.error(error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Get all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find().exec();
+    res.status(200).json(users);
+  } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Get a user by ID
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).exec();
+    if (!user) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Update a user by ID
+app.put('/users/:id', async (req, res) => {
+  try {
+    const { fullName, section, agreeToTerms } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { fullName, section, agreeToTerms },
+      { new: true }
+    ).exec();
+    if (!updatedUser) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Delete a user by ID
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id).exec();
+    if (!deletedUser) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
